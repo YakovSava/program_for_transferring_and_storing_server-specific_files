7066d7baa942ff912f6604bcaeb11ceb791a493a
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-04-06 21:59:34 +0500

    Version 0.2

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import asyncio
+
+from aiohttp.web import Application, RouteTableDef, run_app, Response, json_response, Request
+
+app = Application()
+routes = RouteTableDef()
+
+@routes.get('/')
+async def main_page(request:Request):
+    return Response(status=418)
+
+@routes.get('/file?')
+async def get_file(request:Request):
+    return Response(body=f'{str(request.url).split("?")[-1]}')
+
+@routes.get('/service?')
+async def service_path(request:Request):
+    pass
