594d17375eea8e8ce9a1905e55c117c38dd8937b
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-04-08 10:13:34 +0500

    Version 0.5(!)

--- main.py
+++ main.py
@@ -7,18 +7,39 @@ app = Application()
 routes = RouteTableDef()
 binder = Binder(path='/files')
 
+
 @routes.get('/')
-async def main_page(request:Request):
-    return Response(body='A'+('a'*(100*100)))
+async def main_page(request: Request):
+    return Response(body='A' + ('a' * (100 * 100)))
+
 
 @routes.get('/file?')
-async def get_file(request:Request):
+async def get_file(request: Request):
     file_bytes_data = await binder.get_file(await binder.get_file(str(request.url).split('?')[-1]))
     return Response(body=file_bytes_data)
 
+
 @routes.get('/service?')
-async def service_path(request:Request):
-    return Response(body='Aaaaa')
+async def service_path(request: Request):
+    request_type = str(request.url).split('?')[-1]
+    if request_type == 'getAll':
+        return json_response(
+            data={'response': await binder.get_all_files()}
+        )
+    elif request_type == 'post':
+        return json_response(
+            data={'response': '/downoload'}
+        )
+    else:
+        return json_response(data={'response': 0})
+
+@routes.post('/downoload')
+async def downoload_file(request:Request):
+    try:
+        data = await request.json()
+        await binder.write(data['filename'], data['data'], binary=data['binary'])
+    except: return json_response(data={'response': 0})
+    else: return json_response(data={'response': 1})
 
 if __name__ == '__main__':
     app.add_routes(routes)
