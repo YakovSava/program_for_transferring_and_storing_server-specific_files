a4362073ad77c52028a08cedfc20b75fdcfc5db7
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-05-10 20:00:21 +0500

    Version 0.31
    Release version

--- html/scripts/js.js
+++ html/scripts/js.js
@@ -71,15 +71,15 @@ async function sendFilters() {
 		let filterArray = [];
 		let result = [];
 		while (i < 5) {
-				i++
-				let filter = Number(document.querySelectorAll("input")[i].value);
-				filterArray.push(filter);
+			i++
+			let filter = Number(document.querySelectorAll("input")[i].value);
+			filterArray.push(filter);
 		}
 		while (filterArray.length) {
-				result.push(filterArray.splice(0, 2));
+			 result.push(filterArray.splice(0, 2));
 		}
 		if (result.includes(0)) {
-				alert("Заполните все поля используя числа");
+			alert("Заполните все поля используя числа");
 		} else {
 			let data = {
 				user: "apiKey",
@@ -97,12 +97,18 @@ async function sendFilters() {
 			// console.log(response.response);
 	
 			for (let i = 0; i < response.response.length; i++) {
-				// console.log(response.response[0][i][1])
-					text += `<div class="main__image">
+				if (i % 3 === 0) {
+					text += '<div class="main__image_block">'
+				}
+				text += `<div class="main__image">
 		<img src="${response.response[i].files.a3d}" width="350px" height="350px" id="img_${i}" onmouseover="this.src='${response.response[i].files.two}';" onmouseout="this.src='${response.response[i].files.a3d}';">
-		<a href="#">${response.response[i].project} от ${response.response[i].autor}</a>
+		<a href="/picture/${response.response[i].autor}/${response.response[i].project}">${response.response[i].project} от ${response.response[i].autor}</a>
 	</div>`;
-					imgContainer.innerHTML = text;
+				imgContainer.innerHTML = text;
+
+				if (i % 3 === 2) {
+					text += '</div>'
+				}
 			}
 		};
 	} else {
@@ -193,39 +199,40 @@ async function checkCookie() {
 			inputs[i+1].value = response2.values[div(i, 2)][1];
 		}
 
-			let data = {
-				user: "apiKey",
-				password: "Igor Gygabyte moment",
-				filters: [
-					[0, 123456789],
-					[0, 123456789],
-					[0, 123456789]
-				]
-			};
-			let resp = await fetch(`api?method=getFilesList&data=${JSON.stringify(data)}`);
-			let response = await resp.json();
+		let data = {
+			user: "apiKey",
+			password: "Igor Gygabyte moment",
+			filters: [
+				[0, 123456789],
+				[0, 123456789],
+				[0, 123456789]
+			]
+		};
+		let resp = await fetch(`api?method=getFilesList&data=${JSON.stringify(data)}`);
+		let response = await resp.json();
 
-			// await cookieFilters(data.filters);
-	
-			var imgContainer = document.querySelector('.main__images');
-			var text = '';
+		// await cookieFilters(data.filters);
 
-			// console.log(response.response);
-			text += '<div id="main">'
-	
-			for (let i = 0; i < response.response.length; i++) {
-				// console.log(response.response[0][i][1]);
-				if (div(i, 3) === 1) text += '<div id="main__2">'
-				text += `<div class="main__image">
-		<img src="${response.response[i].files.a3d}" width="350px" height="350px" id="img_${i}" onmouseover="this.src='${response.response[i].files.two}';" onmouseout="this.src='${response.response[i].files.a3d}';">
-		<a href="/picture/${response.response[i].autor}/${response.response[i].project}">${response.response[i].project} от ${response.response[i].autor}</a>
-	</div>`;
-				imgContainer.innerHTML = text;
+		var imgContainer = document.querySelector('.main__images');
+		var text = '';
 
-				if (div(i, 3) === 1) text += '</div>'
+		// console.log(response.response);
+
+		for (let i = 0; i < response.response.length; i++) {
+			console.log(i, i % 3)
+			if (i % 3 === 0) {
+				text += '<div class="main__image_block">'
 			}
+			text += `<div class="main__image">
+	<img src="${response.response[i].files.a3d}" width="350px" height="350px" id="img_${i}" onmouseover="this.src='${response.response[i].files.two}';" onmouseout="this.src='${response.response[i].files.a3d}';">
+	<a href="/picture/${response.response[i].autor}/${response.response[i].project}">${response.response[i].project} от ${response.response[i].autor}</a>
+</div>`;
+			imgContainer.innerHTML = text;
 
-			text += '</div>'
+			if (i % 3 === 2) {
+				text += '</div>'
+			}
+		}
 	}
 }
 
